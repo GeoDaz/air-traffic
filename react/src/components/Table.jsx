60a35507bdabb3f27566f421c654a6565f76fd0d
rbtkay <robert.g.khayat@gmail.com>
2020-11-13 02:32:05 +0100

    page question created

--- react/src/components/Table.jsx
+++ react/src/components/Table.jsx
@@ -1,45 +1,26 @@
-const Table = () => {
+const Table = ({ data, headers }) => {
+    console.log(data);
     return (
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    {headers.map((h, index) => (
+                        <th scope="col" key={index}>
+                            {h}
+                        </th>
+                    ))}
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                {data.map((row, index) => (
+                    <tr>
+                        <th scope="row">{index + 1}</th>
+                        {row.map((col, index) => (
+                                <td>{col}</td>
+                        ))}
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
